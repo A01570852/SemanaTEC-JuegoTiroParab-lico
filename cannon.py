8a4fd39954dfe52cc42f1c3c3c48bb3b1510cc2f
EdgarCastilloRm <69534609+EdgarCastilloRm@users.noreply.github.com>
2020-09-18 11:56:04 -0500

    Juego que nunca termina y documentación
    En este commit se añade la característica que hace que el juego nunca termine y vuelva a cargar lo que sale de la pantalla en el inicio. Además se hace la documentación del código.

--- cannon.py
+++ cannon.py
@@ -1,13 +1,26 @@
+#A00827826 Edgar Castillo
+#A01570852 Luis Martínez
+
+"""
+Programa del juego Cannon o tiro parabólico.
+"""
+
+#Librerías
 from random import randrange
 from turtle import *
 from freegames import vector
 
-ball = vector(-200, -200)
+ball = vector(-200, -200) #Posición inicial de la pelota cuando se lanza.
 speed = vector(0, 0)
-targets = []
+targets = [] #Vector indefinido de objetivos o globos.
+color = ['blue','yellow','green','pink','orange','black','brown','purple'] #Colores que pueden tomar los objetivos.
+index = randrange(8) #Cada vez que se abre un nuevo juego los objetivos toman un color distinto.
+
 
 def tap(x, y):
-    "Respond to screen tap."
+    """Función que hace que la pelota se lance según el click del usuario. Si clickea fuera
+    se interpreta como si estuviera al borde de los límites.
+    """
     if not inside(ball):
         ball.x = -199
         ball.y = -199
@@ -15,37 +28,50 @@ def tap(x, y):
         speed.y = (y + 200) / 25
 
 def inside(xy):
-    "Return True if xy within screen."
+    """Función que denota si una pelota o un objetivo está dentro o fuera de la ventana.
+    Si está dentro regresa True.
+    """
     return -200 < xy.x < 200 and -200 < xy.y < 200
 
 def draw():
-    "Draw ball and targets."
+    """Función en la que se dibujan tanto objetivos como la pelota. Como se dijo antes todos los
+    objetivos tomarán un color distinto cada vez que se corra el código. La pelota se mantendrá con
+    un color blanco.
+    """
     clear()
-
+    
+    #Para objetivos
     for target in targets:
         goto(target.x, target.y)
-        dot(20, 'blue')
+        dot(20, color[index])
 
+    #Para la pelota
     if inside(ball):
         goto(ball.x, ball.y)
-        dot(6, 'red')
+        dot(6, 'white')
 
     update()
 
 def move():
-    "Move ball and targets."
+    """Función que describe el movimiento de los objetivos y la pelota.
+    """
+    
+    #Se crean los globos de manera aleatoria. Se les da una altura específica pero randomizada.
     if randrange(40) == 0:
         y = randrange(-150, 150)
         target = vector(200, y)
         targets.append(target)
 
+    #Se reduce un poco el eje x de los objetivos.
     for target in targets:
         target.x -= 0.5
-
+    
+    #Se reduce la velocidad de la pelota.
     if inside(ball):
-        speed.y -= 0.2
+        speed.y -= 0.2 #Se reduce menos velocidad (Antes era de -0.35)
         ball.move(speed)
-
+    
+    #Líneas de código para eliminar los globos cuando pasa la pelota sobre ellos. Se crea otra lista igual a la de objetivos.
     dupe = targets.copy()
     targets.clear()
 
@@ -54,14 +80,19 @@ def move():
             targets.append(target)
 
     draw()
-
+    
+    #Esto hace que el juego sea infinito y lo que se sale de la ventana vuelva a reaparecer al inicio de la misma
     for target in targets:
         if not inside(target):
-            return
+            target.x = 200
+    
+    #Con esto se aumenta la velocidad de todo el juego. Estaba en 50, fue cambiado a 25. La función move será llamada cada 25 milisegundos.
+    ontimer(move, 15)
+    
 
-    ontimer(move, 25)
-
-setup(420, 420, 370, 0)
+#Características de la ventana.
+setup(420, 420, 370, 0) #Dimensiones
+bgcolor('black') #Color
 hideturtle()
 up()
 tracer(False)
